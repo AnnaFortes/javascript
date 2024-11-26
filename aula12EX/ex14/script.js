76bb975ccfb07145e20d6d5c597b4b2318f387b4
Ana Fortes <miah.fortes@gmail.com>
2024-11-26 16:28:37 -0300

    exercicio horario ok

--- aula12EX/ex14/script.js
+++ aula12EX/ex14/script.js
@@ -1,1 +1,21 @@
-alert('Olá')
+function carregar() {
+var msg = document.getElementById('msg')
+var img = document.getElementById('imagem')
+var data = new Date()
+var hora = data.getHours()
+//var hora = 15
+msg.innerHTML = `Agora são ${hora} horas`
+if (hora >= 0 && hora < 12) {
+     //bom dia
+     img.src = 'foto-manha.png'
+     document.body.style.background = '#feee8c'
+} else if (hora >= 12 && hora <= 18) {
+    //boa tarde
+    img.src = 'foto-tarde.png'
+    document.body.style.background = '#f48300'
+} else {
+    //boa noite
+    img.src = 'foto-noite.png'
+    document.body.style.background = '#233a6c'
+}
+}
