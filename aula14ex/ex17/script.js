31d7929bac9c9bd459fb49014d44a9ef72261187
Ana Fortes <miah.fortes@gmail.com>
2024-11-29 17:00:03 -0300

    desafio tabuada ok, preciso corrigir o alert

--- aula14ex/ex17/script.js
+++ aula14ex/ex17/script.js
@@ -1,1 +1,15 @@
-alert('Olá')
+function tabuada() {
+var tab = Number(document.querySelector('input#num').value)
+
+if (isNaN(tab)) {
+    alert('Por favor, digite um número!')
+    return;
+} 
+var resultado = ''
+
+for (var num = 1; num <= 10; num++) {
+    var res = tab * num
+    resultado += `${tab} x ${num} = ${res}<br>`
+}
+document.querySelector('div#txt').innerHTML = resultado
+}
--- testes/teste.js
+++ testes/teste.js
@@ -1,4 +1,5 @@
 //teste de contagem supondo a entrada do usuario
+/*
 var i = 1
 var p = 3
 var f = 10
@@ -6,6 +7,14 @@ do {
     console.log(`Contando ${i}`)
     i += p
 } while (i <= f)
+*/
+/*
+var tab = 5 
+for (var num = 1; num <= 10; num++) {
+     res = tab * num 
+}
+*/
+console.log('Por favor, digite um número!')
 
 
 
