a9993c3d7c30c9afb929583ee9fb34c97ceba8fe
Ana Fortes <miah.fortes@gmail.com>
2024-11-29 12:45:26 -0300

    desafio 16 ok

--- aula14ex/ex16/script.js
+++ aula14ex/ex16/script.js
@@ -1,1 +1,23 @@
-alert('Olá')
+function contar() {
+    var ini = Number(document.querySelector('input#ini').value)
+    var fim = Number(document.querySelector('input#fim').value)
+    var pas = Number(document.querySelector('input#pas').value)
+    //var res = document.querySelector('div#res').value
+    if (ini <= 0 || isNaN(ini)) {
+        alert('Inválido! Digite um número válido. Considerando: INÍCIO 1')
+        ini = 1;
+    }
+    
+    if (pas <= 0 || isNaN(pas)) {
+        alert('Passo inválido! Considerando: PASSO 1')
+        pas = 1;
+    }
+    var resul = ''
+
+    do {
+        resul += `${ini} `
+        ini += pas
+    } while (ini <= fim)   
+    
+    document.querySelector('div#res').innerHTML = `Contando: ${resul}`
+}
---
+++ testes/teste.js
@@ -0,0 +1,11 @@
+//teste de contagem supondo a entrada do usuario
+var i = 1
+var p = 3
+var f = 10
+do {
+    console.log(`Contando ${i}`)
+    i += p
+} while (i <= f)
+
+
+
