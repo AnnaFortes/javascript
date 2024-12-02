913269534935a84fb6f630a64f2e2adc95d240c1
Ana Fortes <miah.fortes@gmail.com>
2024-12-02 16:40:42 -0300

    curso de js finalizado

--- desafios/desafio2/modelo/script.js
+++ desafios/desafio2/modelo/script.js
@@ -1,23 +1,39 @@
 let numNovo = []
+
 function numero() {
     let num = Number(document.querySelector('input#num').value)
-    numNovo.push(num)
-    if (num > 1 || num <= 100 || isNaN(num) || num == '') {
+    let select = document.getElementById('add')
+    if (num < 1 || num > 100 || isNaN(num) || numNovo.includes(num)) {
         alert('Valor inválido ou já encontrado na lista!')
-    } 
+    } else {
+        numNovo.push(num)
+
+        let option = document.createElement('option')
+        option.textContent = `Valor ${num} adicionado`
+        select.appendChild(option)
+    }
+    document.querySelector('input#num').value = '' //limpa o campo
+    document.querySelector('input#num').focus()
 }
 
-function fim(numNovo) {
-    for (let comprimento in numNovo[comprimento]) {
-        let optionTodos = document.getElementById('option')
-        optionTodos.textContent = `Ao todo temos ${numNovo[comprimento]} números cadastrados`
-        select.appendChild(optionTodos)
-    }
+function fim() {
+    if (numNovo.length === 0) {
+        alert('Adicione valores antes de finalizar!')
+        return
+    } 
 
+    let select = document.getElementById('add')
+
+    select.innerHTML = ''
+
+    //total de numeros
+    let optionTodos = document.createElement('option')
+    optionTodos.textContent = `Ao todo temos ${numNovo.length} números cadastrados`
+    select.appendChild(optionTodos)
+    
+    //maior e menor
     let maiorNum = Math.max(...numNovo)
     let menorNum = Math.min(...numNovo)
-
-    let select = document.getElementById('select#add')
 
     let optionMaior = document.createElement('option')
     optionMaior.textContent = `O maior valor informado foi ${maiorNum}`
@@ -27,16 +43,15 @@ function fim(numNovo) {
     optionMenor.textContent = `O menor valor informado foi ${menorNum}`
     select.appendChild(optionMenor)
 
-    let soma = 0 //armazena a soma dos numeros(começa fora do loop)
-        for (let pos in num) { // para a posição em cada numero
-        soma += num[pos] //soma = soma + numero da posição
+    let soma = numNovo.reduce((total, valor) => total + valor, 0) //reduce faz o calculo de todo o array, total é o acumulador que vai começar em 0, valor é cada numero do array
 
     let optionSoma = document.createElement('option')
     optionSoma.textContent = `Somando todos os valores temos ${soma}`
     select.appendChild(optionSoma)
 
+    let media = soma/numNovo.length
+
     let optionMedia = document.createElement('option')
-    optionMedia.textContent = `A média dos valores digitados é ${soma / pos}`
+    optionMedia.textContent = `A média dos valores digitados é ${media.toFixed(2)}`
     select.appendChild(optionMedia)
 }     
-} 
